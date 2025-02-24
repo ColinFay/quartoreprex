3da5b6171019e3db54c54613952e1fb23cca249e
colin <colin@thinkr.fr>
2025-02-24 21:01:14 +0100

    feat: reprex

---
+++ script.R
@@ -0,0 +1,28 @@
+options(
+  future.rng.onMisuse = "ignore"
+)
+future::plan(future::multisession)
+
+for (i in 1:50){
+  write(file = sprintf("test%s.qmd", i),
+        sprintf("---
+title: 'test%s'
+---
+
+## Quarto
+
+Quarto enables you to weave together content and executable code into a finished presentation. To learn more about Quarto presentations see <https://quarto.org/docs/presentations/>.
+", i)
+  )
+}
+
+list.files(pattern = "\\.qmd$", full.names = TRUE) |>
+  furrr::future_map(
+    .progress = TRUE,
+    \(x){
+      quarto::quarto_render(
+        x,
+        quiet = FALSE
+      )
+    }
+  )
